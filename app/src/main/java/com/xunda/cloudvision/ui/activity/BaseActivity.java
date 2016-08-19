990d2a4113a7e318d47e5b5334387165b4f58178
XIAOYING <yinglovezhuzhu@139.com>
2016-08-19 22:54:24 +0800

    Initial commit

---
+++ app/src/main/java/com/xunda/cloudvision/ui/activity/BaseActivity.java
@@ -0,0 +1,117 @@
+package com.xunda.cloudvision.ui.activity;
+
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningAppProcessInfo;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class BaseActivity extends FragmentActivity implements View.OnClickListener {
+	
+	protected final String TAG = getClass().getSimpleName();
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
+	
+	@Override
+	protected void onStop() {
+		super.onStop();
+	}
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+	
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/**
+     * 显示一个短Toast
+     * @param text
+     */
+    protected void showShortToast(CharSequence text) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * 显示一个短Toast
+     * @param resId
+     */
+    protected void showShortToast(int resId) {
+        Toast.makeText(this, resId, Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * 显示一个长Toast
+     * @param text
+     */
+    protected void showLongToast(CharSequence text) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * 显示一个长Toast
+     * @param resId
+     */
+    protected void showLongToast(int resId) {
+        Toast.makeText(this, resId, Toast.LENGTH_SHORT).show();
+    }
+	
+	
+	/**
+     * 程序是否在前台运行
+     * 
+     * @return
+     */
+    public boolean isAppOnForeground() {
+            // Returns a list of application processes that are running on the
+            // device
+             
+            ActivityManager activityManager = (ActivityManager) getApplicationContext().getSystemService(Context.ACTIVITY_SERVICE);
+            String packageName = getApplicationContext().getPackageName();
+
+            List<RunningAppProcessInfo> appProcesses = activityManager
+                            .getRunningAppProcesses();
+            if (appProcesses == null)
+                    return false;
+
+            for (RunningAppProcessInfo appProcess : appProcesses) {
+                    // The name of the process that this object is associated with.
+                    if (appProcess.processName.equals(packageName)
+                                    && appProcess.importance == RunningAppProcessInfo.IMPORTANCE_FOREGROUND) {
+                            return true;
+                    }
+            }
+
+            return false;
+    }
+
+	/**
+	 * 退出当前Activity
+	 * @param resultCode 结果吗
+	 * @param extras 额外传递数据
+	 */
+	protected void finish(int resultCode, Bundle extras) {
+		Intent data = new Intent();
+		if(null != extras && !extras.isEmpty()) {
+			data.putExtras(extras);
+		}
+		setResult(resultCode, data);
+		finish();
+	}
+}
---
+++ app/src/main/java/com/xunda/cloudvision/ui/activity/MainActivity.java
@@ -0,0 +1,14 @@
+package com.xunda.cloudvision.ui.activity;
+
+import android.os.Bundle;
+
+import com.xunda.cloudvision.R;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
