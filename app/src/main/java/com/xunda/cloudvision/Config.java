3cf62947a18c10cb9edf403e6eccfc5b1e6d7b73
XIAOYING <yinglovezhuzhu@139.com>
2016-08-20 09:24:32 +0800

    commit

--- app/src/main/java/com/xunda/cloudvision/Config.java
+++ app/src/main/java/com/xunda/cloudvision/Config.java
@@ -5,4 +5,10 @@ package com.xunda.cloudvision;
  * Created by yinglovezhuzhu@gmail.com on 2016/8/20.
  */
 public class Config {
+
+    /** SP文件名称：配置 **/
+    public final static String SP_FILE_CONFIG = "sp_file_config";
+
+    /** SP文件名称：缓存 **/
+    public static final String SP_FILE_CACHE = "sp_file_cache";
 }
