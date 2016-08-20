973d641585f1327d95135453a79e2683e08ea288
XIAOYING <yinglovezhuzhu@139.com>
2016-08-20 09:19:55 +0800

    commit

---
+++ app/src/main/java/com/xunda/cloudvision/CVApplication.java
@@ -0,0 +1,9 @@
+package com.xunda.cloudvision;
+
+import android.app.Application;
+
+/**
+ * Created by yinglovezhuzhu@gmail.com on 2016/8/20.
+ */
+public class CVApplication extends Application {
+}
---
+++ app/src/main/java/com/xunda/cloudvision/Config.java
@@ -0,0 +1,8 @@
+package com.xunda.cloudvision;
+
+/**
+ * 配置文件
+ * Created by yinglovezhuzhu@gmail.com on 2016/8/20.
+ */
+public class Config {
+}
