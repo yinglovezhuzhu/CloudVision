1f5cb62611f3f39fefb1b2c7c89a68e6f521dc01
XIAOYING <yinglovezhuzhu@139.com>
2016-09-13 22:39:43 +0800

    激活界面布局

---
+++ app/src/main/java/com/xunda/cloudvision/ui/activity/ActivateActivity.java
@@ -0,0 +1,15 @@
+package com.xunda.cloudvision.ui.activity;
+
+import android.os.Bundle;
+
+/**
+ * 激活页面
+ * Created by yinglovezhuzhu@gmail.com on 2016/9/13.
+ */
+public class ActivateActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
