163ad99fb1c276de41459ac988e7c14950446e56
XIAOYING <yinglovezhuzhu@139.com>
2016-09-13 22:43:21 +0800

    激活界面布局

--- app/src/main/java/com/xunda/cloudvision/ui/activity/MainActivity.java
+++ app/src/main/java/com/xunda/cloudvision/ui/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.xunda.cloudvision.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.xunda.cloudvision.R;
@@ -10,5 +11,7 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startActivity(new Intent(this, ActivateActivity.class));
     }
 }
