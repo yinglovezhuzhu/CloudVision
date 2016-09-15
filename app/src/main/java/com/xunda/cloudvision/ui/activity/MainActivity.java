1b88dab79bb8d68dac3e19cef6dac321f3d31e3d
XIAOYING <yinglovezhuzhu@139.com>
2016-09-15 21:21:01 +0800

    主界面布局

--- app/src/main/java/com/xunda/cloudvision/ui/activity/MainActivity.java
+++ app/src/main/java/com/xunda/cloudvision/ui/activity/MainActivity.java
@@ -2,6 +2,9 @@ package com.xunda.cloudvision.ui.activity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
 import android.widget.CompoundButton;
 import android.widget.RadioGroup;
 import android.widget.ToggleButton;
@@ -33,6 +36,9 @@ public class MainActivity extends BaseActivity {
 
     private void initView() {
         final RadioGroup rgMenuItems = (RadioGroup) findViewById(R.id.rg_main_menu_items);
+        final ToggleButton tBtnMainMenu = (ToggleButton) findViewById(R.id.tbtn_main_menu);
+        final Animation menuItemShowAnimation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.anim_main_menu_items_show);
+        final Animation menuItemHideAnimation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.anim_main_menu_items_hide);
         rgMenuItems.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
@@ -40,14 +46,45 @@ public class MainActivity extends BaseActivity {
             }
         });
 
-        final ToggleButton tBtnMainMenu = (ToggleButton) findViewById(R.id.tbtn_main_menu);
         tBtnMainMenu.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
-                rgMenuItems.setVisibility(checked ? View.VISIBLE : View.GONE);
+                if(checked) {
+                    rgMenuItems.startAnimation(menuItemShowAnimation);
+                    rgMenuItems.setVisibility(View.VISIBLE);
+                } else {
+                    rgMenuItems.startAnimation(menuItemHideAnimation);
+                }
+
                 rgMenuItems.check(0);
             }
         });
         findViewById(R.id.btn_main_menu_home).setOnClickListener(this);
+        menuItemShowAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                tBtnMainMenu.setBackgroundResource(R.drawable.layer_list_bg_main_menu_on_open);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) { }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) { }
+        });
+
+        menuItemHideAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) { }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                rgMenuItems.setVisibility(View.GONE);
+                tBtnMainMenu.setBackgroundResource(R.drawable.layer_list_bg_main_menu);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) { }
+        });
     }
 }
