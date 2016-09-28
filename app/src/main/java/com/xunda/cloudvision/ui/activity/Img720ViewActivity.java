ee8bcb98b3b51912c45d606143b8be2846463523
yinglovezhuzhu <yinglovezhuzhu@139.com>
2016-09-28 18:42:24 +0800

    产品详情

---
+++ app/src/main/java/com/xunda/cloudvision/ui/activity/Img720ViewActivity.java
@@ -0,0 +1,16 @@
+package com.xunda.cloudvision.ui.activity;
+
+import android.os.Bundle;
+
+/**
+ * 720度看图
+ * Created by yinglovezhuzhu@gmail.com on 2016/9/28.
+ */
+
+public class Img720ViewActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
