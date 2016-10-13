549bd9dfbbb3a2f3142c30764ec4623060539e75
XIAOYING <yinglovezhuzhu@139.com>
2016-10-14 00:09:35 +0800

    产品详情 UI优化

--- app/src/main/java/com/xunda/cloudvision/ui/activity/ProductDetailActivity.java
+++ app/src/main/java/com/xunda/cloudvision/ui/activity/ProductDetailActivity.java
@@ -1,17 +1,32 @@
 package com.xunda.cloudvision.ui.activity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Message;
 import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.AbsListView;
+import android.widget.ListView;
 
 import com.xunda.cloudvision.R;
 import com.xunda.cloudvision.bean.resp.QueryProductDetailResp;
+import com.xunda.cloudvision.presenter.ProductDetailAttrAdapter;
 import com.xunda.cloudvision.presenter.ProductDetailPresenter;
 import com.xunda.cloudvision.ui.adapter.ProductDetailImgAdapter;
+import com.xunda.cloudvision.ui.fragment.WebViewFragment;
+import com.xunda.cloudvision.ui.widget.NoScrollGridView;
 import com.xunda.cloudvision.ui.widget.PageControlBar;
+import com.xunda.cloudvision.utils.NetworkManager;
 import com.xunda.cloudvision.view.IProductDetailView;
 
 /**
@@ -21,10 +36,18 @@ import com.xunda.cloudvision.view.IProductDetailView;
 
 public class ProductDetailActivity extends BaseActivity implements IProductDetailView {
 
+    private ProductDetailPresenter mProductDetailPresenter;
+
     private PageControlBar mPageIndicator;
     private ProductDetailImgAdapter mImgAdapter;
 
-    private ProductDetailPresenter mProductDetailPresenter;
+    private NoScrollGridView  mGvColor;
+    private NoScrollGridView mGvSize;
+    private View mDetailLoadingView;
+    private WebView mWebView;
+
+    private ProductDetailAttrAdapter mColorAttrAdapter;
+    private ProductDetailAttrAdapter mSizeAttrAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -120,6 +143,17 @@ public class ProductDetailActivity extends BaseActivity implements IProductDetai
                 }
             }
         });
+        mGvColor = (NoScrollGridView) findViewById(R.id.gv_product_detail_color);
+        mColorAttrAdapter = new ProductDetailAttrAdapter(this);
+        mGvColor.setAdapter(mColorAttrAdapter);
+        mGvSize = (NoScrollGridView) findViewById(R.id.gv_product_detail_size);
+        mSizeAttrAdapter = new ProductDetailAttrAdapter(this);
+        mGvSize.setAdapter(mSizeAttrAdapter);
+
+        mDetailLoadingView = findViewById(R.id.ll_product_detail_loading);
+        mWebView = (WebView) findViewById(R.id.wv_product_detail);
+        settingWebView(mWebView);
+        mWebView.loadUrl("https://www.baidu.com/");
     }
 
     @Override
@@ -135,5 +169,94 @@ public class ProductDetailActivity extends BaseActivity implements IProductDetai
     @Override
     public void onQueryProductDetailResult(QueryProductDetailResp result) {
 
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private void settingWebView(WebView webView) {
+        // Javascript enabled on webview，不加这个页面可能加载不出来
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setSupportZoom(false);
+        webView.getSettings().setAllowFileAccess(true);
+        webView.getSettings().setSupportMultipleWindows(true);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        // 根据网络状态来设定缓存模式
+        if(NetworkManager.getInstance().isNetworkConnected()) {
+            webView.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT); // 缓存模式：默认模式
+        } else {
+            webView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ONLY); // 缓存模式：只加载缓存
+        }
+        webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+
+        webView.setWebChromeClient(new CusWebChromeClient());
+        webView.setWebViewClient(new CusWebViewClient());
+    }
+
+    /**
+     * WebView 设置
+     */
+    private class CusWebChromeClient extends WebChromeClient {
+        private WebView mmChildView = null;
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            super.onProgressChanged(view, newProgress);
+        }
+
+        @SuppressLint("SetJavaScriptEnabled")
+        @Override
+        public boolean onCreateWindow(WebView view, boolean isDialog, boolean isUserGesture, Message resultMsg) {
+            mmChildView = new WebView(ProductDetailActivity.this);
+            settingWebView(mmChildView);
+            view.addView(mmChildView);
+
+            WebView.WebViewTransport transport = (WebView.WebViewTransport) resultMsg.obj;
+            transport.setWebView(mmChildView);
+            resultMsg.sendToTarget();
+
+            return true;
+        }
+
+        @Override
+        public void onCloseWindow(WebView window) {
+            if (null != mmChildView) {
+                mmChildView.setVisibility(View.GONE);
+                window.removeView(mmChildView);
+            }
+        }
+    }
+
+    /**
+     * WebView 设置
+     */
+    private class CusWebViewClient extends WebViewClient {
+        //在点击请求的是链接是才会调用，重写此方法返回true表明点击网页里面的链接还是在当前的webview里跳转，不跳到浏览器那边。
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
+        //重写此方法可以让webview处理https请求。
+        @Override
+        public void onReceivedSslError(WebView view, SslErrorHandler handler, android.net.http.SslError error) {
+            handler.proceed();
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            mDetailLoadingView.setVisibility(View.GONE);
+        }
+
+        @Override
+        public void onReceivedHttpError(WebView view, WebResourceRequest request, WebResourceResponse errorResponse) {
+            super.onReceivedHttpError(view, request, errorResponse);
+        }
+
+        @Override
+        public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+            super.onReceivedError(view, request, error);
+        }
     }
 }
