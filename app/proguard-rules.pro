952b424b401ad0ca842b39b0255bb4d45d83af78
XIAOYING <yinglovezhuzhu@139.com>
2016-10-27 23:29:31 +0800

    极光推送

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -15,3 +15,9 @@
 #-keepclassmembers class fqcn.of.javascript.interface.for.webview {
 #   public *;
 #}
+
+-dontoptimize
+-dontpreverify
+
+-dontwarn cn.jpush.**
+-keep class cn.jpush.** { *; }
--- app/src/main/java/com/vrcvp/cloudvision/CVApplication.java
+++ app/src/main/java/com/vrcvp/cloudvision/CVApplication.java
@@ -5,6 +5,8 @@ import android.app.Application;
 import com.vrcvp.cloudvision.utils.DataManager;
 import com.vrcvp.cloudvision.utils.NetworkManager;
 
+import cn.jpush.android.api.JPushInterface;
+
 /**
  * 程序Application
  * Created by yinglovezhuzhu@gmail.com on 2016/8/20.
@@ -17,5 +19,7 @@ public class CVApplication extends Application {
 
         NetworkManager.getInstance().initialized(this);
         DataManager.getInstance().initialize(this);
+        JPushInterface.setDebugMode(BuildConfig.DEBUG);
+        JPushInterface.init(this);
     }
 }
