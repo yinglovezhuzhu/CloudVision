7c131f76015a5cfe5b5c65beab87c8a19a92c22c
yinglovezhuzhu <yinglovezhuzhu@139.com>
2016-11-10 18:15:42 +0800

    首页没有数据

--- app/src/main/java/com/vrcvp/cloudvision/CVApplication.java
+++ app/src/main/java/com/vrcvp/cloudvision/CVApplication.java
@@ -4,9 +4,13 @@ import android.app.Application;
 
 import com.vrcvp.cloudvision.db.WeatherDBHelper;
 import com.vrcvp.cloudvision.utils.DataManager;
+import com.vrcvp.cloudvision.utils.LogUtils;
 import com.vrcvp.cloudvision.utils.NetworkManager;
 
+import java.util.Set;
+
 import cn.jpush.android.api.JPushInterface;
+import cn.jpush.android.api.TagAliasCallback;
 
 /**
  * 程序Application
@@ -23,5 +27,11 @@ public class CVApplication extends Application {
         DataManager.getInstance().initialize(this);
         JPushInterface.setDebugMode(BuildConfig.DEBUG);
         JPushInterface.init(this);
+        JPushInterface.setAlias(this, "AAAAAAAAAAAAAAAAA", new TagAliasCallback() {
+            @Override
+            public void gotResult(int i, String s, Set<String> set) {
+                LogUtils.e("XXXXXXX", i + "<>" + s );
+            }
+        });
     }
 }
